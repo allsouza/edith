4d3f46126b95cb950bed4791736c471a729f5e7d
Glitch (hello-express) <none>
2021-08-10 14:48:25 +0000

    🗽🎌 Checkpoint ./pr-review.js:34743062/1557 ./app.js:34743062/3781

--- pr-review.js
+++ pr-review.js
@@ -1,4 +1,65 @@
 
 class PRReview {
-  static  initialModal()
+  static async initialModal(ack, payload, context, app) {
+    // Acknowledge the command request
+  ack();
+
+  try {
+    const result = await app.client.views.open({
+      token: context.botToken,
+      // Pass a valid trigger_id within 3 seconds of receiving it
+      trigger_id: payload.trigger_id,
+      // View payload
+      view: {
+        type: 'modal',
+        // View identifier
+        callback_id: 'view_1',
+        title: {
+          type: 'plain_text',
+          text: 'Modal title'
+        },
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: 'Welcome to a modal with _blocks_'
+            },
+            accessory: {
+              type: 'button',
+              text: {
+                type: 'plain_text',
+                text: 'Click me!'
+              },
+              action_id: 'button_abc'
+            }
+          },
+          {
+            type: 'input',
+            block_id: 'test_input',
+            label: {
+              type: 'plain_text',
+              text: 'What are your hopes and dreams?'
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'dreamy_input',
+              multiline: true
+            }
+          }
+        ],
+        submit: {
+          type: 'plain_text',
+          text: 'Submit'
+        }
+      }
+    });
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+  }
 }
+
+module.exports = PRReview;
