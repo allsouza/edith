6f8b164dff21a58dd19527a2560c13cc9987dfc0
Glitch (hello-express) <none>
2021-08-11 02:02:00 +0000

    👱🌷 Checkpoint ./pr-review.js:34743062/8103

--- pr-review.js
+++ pr-review.js
@@ -3,116 +3,141 @@ class PRReview {
     // Acknowledge the command request
     ack();
     try {
-      const result = await app.client.views.open(
-      { 
+      const result = await app.client.views.open({
         token: context.botToken,
-      // Pass a valid trigger_id within 3 seconds of receiving it
-      trigger_id: payload.trigger_id,
-      // View payload
-      view: {
-        type: 'modal',
-        // View identifier
-        callback_id: 'view_1',
-        title: {
-          type: 'plain_text',
-          text: 'PR Review'
-        },
-        blocks: [
-          {
-            type: 'input',
-            block_id: 'pr_summary',
-            label: {
-              type: 'plain_text',
-              text: 'PR Summary'
-            },
-            element: {
-              type: 'plain_text_input',
-              action_id: 'summary_input',
-              multiline: false
-            }
+        // Pass a valid trigger_id within 3 seconds of receiving it
+        trigger_id: payload.trigger_id,
+        // View payload
+        view: {
+          type: "modal",
+          // View identifier
+          callback_id: "view_1",
+          title: {
+            type: "plain_text",
+            text: "PR Review"
           },
-          {
-            type: 'input',
-            block_id: 'pr_service',
-            label: {
-              type: 'plain_text',
-              text: 'Service'
+          blocks: [
+            {
+              type: "input",
+              block_id: "pr_summary",
+              label: {
+                type: "plain_text",
+                text: "PR Summary"
+              },
+              element: {
+                type: "plain_text_input",
+                action_id: "summary_input",
+                multiline: false
+              }
             },
-            element: {
-              type: 'plain_text_input',
-              action_id: 'service_input',
-              multiline: false
-            }
-          },
-          {
-            type: 'input',
-            block_id: 'pr_link',
-            label: {
-              type: 'plain_text',
-              text: 'PR Link'
+            {
+              type: "input",
+              block_id: "pr_service",
+              label: {
+                type: "plain_text",
+                text: "Service"
+              },
+              element: {
+                type: "plain_text_input",
+                action_id: "service_input",
+                multiline: false
+              }
             },
-            element: {
-              type: 'plain_text_input',
-              action_id: 'link_input',
-              multiline: false
-            }
-          },
-          {
-            type: 'input',
-            block_id: 'pr_notes',
-            optional: true,
-            label: {
-              type: 'plain_text',
-              text: 'PR Notes'
+            {
+              type: "input",
+              block_id: "pr_link",
+              label: {
+                type: "plain_text",
+                text: "PR Link"
+              },
+              element: {
+                type: "plain_text_input",
+                action_id: "link_input",
+                multiline: false
+              }
             },
-            element: {
-              type: 'plain_text_input',
-              action_id: 'notes_input',
-              multiline: true
-            }
-          },
-          {
-            type: 'input',
-            block_id: 'channel_select',
-            optional: true,
-            label: {
-              type: 'plain_text',
-              text: 'PR Review Request to be posted on '
+            {
+              type: "input",
+              block_id: "pr_notes",
+              optional: true,
+              label: {
+                type: "plain_text",
+                text: "PR Notes"
+              },
+              element: {
+                type: "plain_text_input",
+                action_id: "notes_input",
+                multiline: true
+              }
             },
-            element: {
-              action_id: 'channel_select',
-              type: 'conversations_select',
-              response_url_enabled: true,
-              default_to_current_conversation: true
+            {
+              type: "input",
+              block_id: "channel_select",
+              optional: true,
+              label: {
+                type: "plain_text",
+                text: "PR Review Request to be posted on "
+              },
+              element: {
+                action_id: "channel_select",
+                type: "conversations_select",
+                response_url_enabled: true,
+                default_to_current_conversation: true
+              }
             }
+          ],
+          submit: {
+            type: "plain_text",
+            text: "Submit"
           }
-        ],
-        submit: {
-          type: 'plain_text',
-          text: 'Submit'
         }
-      }
-  });
+      });
       console.log(result);
     } catch (error) {
       console.error(error);
     }
   }
-  
+
   static async postPRReviewRequest(id, data, app) {
     try {
       // debugger
       const message = "<@{here}>Hiiii";
       console.log("Inside post message");
-      debugger
+      debugger;
       const result = await app.client.chat.postMessage({
         token: process.env.SLACK_BOT_TOKEN,
         channel: id,
-        text: "<@U029MQ7BJ5R> Hi there."
+        blocks: [
+          {
+            type: "divider"
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text:
+                "Hey team, <@U029MQ7BJ5R> would like your help reviewing their Pull Request for <${message}>. \n Summary: <summary>"
+            },
+            accessory: {
+              type: "button",
+              text: {
+                type: "plain_text",
+                text: "Take a look :eyes:",
+                emoji: true
+              },
+              value: "click_me_123",
+              url: "https://google.com",
+              action_id: "button-action"
+            }
+          },
+          {
+            type: "divider"
+          }
+        ]
       });
-      console.log(result)
-    } catch(error) {
-      console.error(error)
+      console.log(result);
+    } catch (error) {
+      console.error(error);
     }
   }
 }
