3965ba32a66247ad525066b79e4e7d41addf04c2
Glitch (hello-express) <none>
2021-08-16 17:16:41 +0000

    💾🎨 Checkpoint ./app.js:34743062/737

--- app.js
+++ app.js
@@ -8,23 +8,27 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
-// Listen for a slash command invocation
+// Opens the modal to create a PR Review request
 app.command("/pr_review", async ({ ack, payload, context }) => {
   PRReview.initialModal(ack, payload, context, app);
 });
 
-app.command("/view_prs", async ({ ack, payload, context }) => {
-  ack();
-  PRReview.fetchPendingPRs(payload, app);
-});
-
+// Logic that happens once the modal submit button is selected
 app.view("pr_review_modal_view", ({ ack, body, view }) => {
   ack();
   PRReview.postPRReviewRequest(body.user.id, view, app);
 });
 
+// Displays all the open PR Reviews in the channel
+app.command("/view_prs", async ({ ack, payload, context }) => {
+  ack();
+  PRReview.fetchPendingPRs(payload, app);
+});
+
+// Simply ack when pr link button is clicked
 app.action("link-button-action", ({ ack }) => ack());
 
+// Flow to set PR Review as REVIEWED from /view_prs command
 app.action("review-pr-action", ({ ack, body, client }) => {
   ack();
   PRReview.takeAction(body, client);
