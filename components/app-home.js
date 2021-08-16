cdae0af323f7418d650a9a74bfabb797d0ef361b
Glitch (hello-express) <none>
2021-08-16 22:39:22 +0000

    👣🗼 Checkpoint ./components/app-home.js:34743062/255

--- components/app-home.js
+++ components/app-home.js
@@ -1,5 +1,6 @@
 class AppHome {
   static async open(event, client, context) {
+    const channels = await client.users.conversations({token: "xoxb-2324594394165-2369581715764-ZWxc6YA9YPzkgrkQ6bDOrKjK", user: "U029MQ7BJ5R", types: "public_channel, private_channel"}).channels;
     debugger;
     console.log(event)
   }
