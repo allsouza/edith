51008014b327d19a8ab6b671e5d7b085c548275e
Glitch (hello-express) <none>
2021-08-16 23:25:09 +0000

    👰😽 Checkpoint ./components/app-home.js:34743062/1468

--- components/app-home.js
+++ components/app-home.js
@@ -1,17 +1,22 @@
-const { MongoDB } = require('./db.js');
+const { MongoDB } = require("./db.js");
 
 class AppHome {
   static async open(event, client, context) {
-    let channels = await client.users.conversations({token: "xoxb-2324594394165-2369581715764-ZWxc6YA9YPzkgrkQ6bDOrKjK", user: "U029MQ7BJ5R", types: "public_channel, private_channel"});
+    let channels = await client.users.conversations({
+      token: process.env.SLACK_BOT_TOKEN,
+      user: event.user,
+      types: "public_channel, private_channel"
+    });
     channels = channels.channels;
-debugger
     const PRReviews = {};
-    for( const channel of channels){
+    for (const channel of channels) {
       const data = await MongoDB.listChannelPRs(channel.id);
       if (data.length > 0) PRReviews[channel.id] = data;
     }
-        debugger;
-    console.log(event)
+
+    const result = client.views.publish({
+      user_id: event.user
+    });
   }
 }
 
