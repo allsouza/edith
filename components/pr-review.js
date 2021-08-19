8ccd5a145b39e0fac78cf0811f7dbd0be535f868
Glitch (hello-express) <none>
2021-08-19 04:29:27 +0000

    🕰🌅 Checkpoint ./components/pr-review.js:34743062/11776

--- components/pr-review.js
+++ components/pr-review.js
@@ -370,11 +370,189 @@ class PRReview {
   /*
     Lists open review requests in the channel
   */
-  static async fetchPendingPRsModal(payload, app) {
-    const channel_id = payload.channel_id;
-    const channel_name = payload.channel_name;
-    const user_id = payload.user_id;
-    const data = await MongoDB.listChannelPRs(channel_id);
+  static async fetchPendingPRsModal(payload, app) {    
+    payload.channel = {
+      id: payload.channel_id,
+      name: payload.channel_name
+    }
+    payload.user = {
+      id: payload.user_id
+    }
+
+    const viewBlocks = await createOpenReviewsViewBlock(payload);
+    // Returns result to user
+    app.client.views.open({
+      token: token,
+      trigger_id: payload.trigger_id,
+      view: {
+        type: "modal",
+        title: {
+          type: "plain_text",
+          text: `Open PR Review Requests`,
+          emoji: true
+        },
+        close: {
+          type: "plain_text",
+          text: "Close",
+          emoji: true
+        },
+        blocks: viewBlocks
+      }
+    });
+  }
+
+  /*
+    Updates the request status according to the reaction received
+  */
+  static async computeReaction(event, client) {
+    const channel = event.item.channel;
+    if (event.reaction == REVIEWED || event.reaction == APPROVED) {
+      const dbEntry = await MongoDB.findPR(channel, event.item.ts);
+      if (dbEntry) {
+        client.chat.postMessage({
+          token: token,
+          channel: dbEntry.author,
+          text: `:${event.reaction}: Your PR was ${event.reaction} by <@${event.user}>`
+        });
+
+        switch (event.reaction) {
+          case REVIEWED:
+            await MongoDB.updateStatus(channel, dbEntry._id, REVIEWED);
+            break;
+          case APPROVED:
+            await MongoDB.updateStatus(channel, dbEntry._id, APPROVED);
+            break;
+        }
+      }
+    }
+  }
+
+  /*
+    Prepares object payload to send to update status when a button is pressed
+  */
+  static async takeAction(body, client) {
+    const event = {
+      reaction: body.actions[0].action_id.includes("review")
+        ? REVIEWED
+        : APPROVED,
+      item: {
+        ts: body.actions[0].value,
+        channel: body.channel.id
+      },
+      user: body.user.id
+    };
+    this.computeReaction(event, client);
+  }
+  
+  /*
+    Prepares object payload to send to update status when a button is pressed in modal
+  */
+  static async takeActionModal(body, client) {
+    const parsedValues = JSON.parse(body.actions[0].value)
+    body.channel = {
+      id: parsedValues.channel_id,
+      channel_name: parsedValues.channel_name
+    }
+    debugger
+    const event = {
+      reaction: body.actions[0].action_id.includes("review")
+        ? REVIEWED
+        : APPROVED,
+      item: {
+        ts: parsedValues.pr_post_id,
+        channel: body.channel.id
+      },
+      user: body.user.id
+    };
+    await this.computeReaction(event, client);
+    const viewBlocks = await createOpenReviewsViewBlock(body);
+    await client.views.update({
+      token: token,
+      view_id: body.view.id,
+      view: {
+        type: "modal",
+        title: {
+          type: "plain_text",
+          text: `Open PR Review Requests`,
+          emoji: true
+        },
+        close: {
+          type: "plain_text",
+          text: "Close",
+          emoji: true
+        },
+        blocks: viewBlocks
+      }
+    });
+  }
+
+  /*
+    Removes request from DB and sends message in thread to say the PR was merged
+  */
+  static async mergedPR(data, client) {
+    const isFromAppHome = data.container.type == "view";
+    const body = isFromAppHome ? AppHome.normalizeBody(data) : data;
+    const dbEntry = await MongoDB.findPR(
+      body.channel.id,
+      body.actions[0].value
+    );
+    if (!dbEntry) return; // If entry not found do nothing
+    if (dbEntry.author == body.user.id) {
+      await MongoDB.finalizePR(body.channel.id, body.actions[0].value);
+      client.chat.postMessage({
+        token: token,
+        text: ":checkered_flag: Pull Request merged. Thank you all!",
+        channel: body.channel.id,
+        thread_ts: body.actions[0].value
+      });
+      if (isFromAppHome) {
+        console.log("Should open the other view");
+        client.views.push({
+          token: token,
+          trigger_id: body.trigger_id,
+          view: {
+            type: "modal",
+            title: {
+              type: "plain_text",
+              text: "PR Review Complete",
+              emoji: true
+            },
+            close: {
+              type: "plain_text",
+              text: "Close",
+              emoji: true
+            },
+            blocks: [
+              {
+                type: "section",
+                text: {
+                  type: "mrkdwn",
+                  text:
+                    ":checkered_flag: Review request marked as *Merged* and removed from queue."
+                }
+              }
+            ]
+          }
+        });
+      }
+    } else {
+      client.chat.postEphemeral({
+        token: token,
+        text: ":octagonal_sign: Only the author can set as Merged",
+        channel: body.channel.id,
+        thread_ts: body.actions[0].value,
+        user: body.user.id
+      });
+    }
+  }
+}
+
+async function createOpenReviewsViewBlock(payload) {
+  debugger
+  const channel_id = payload.channel.id;
+  const channel_name = payload.channel.name;
+  const user_id = payload.user.id;
+  const data = await MongoDB.listChannelPRs(channel_id);
     const blocks =
       data.length > 0
         ? []
@@ -462,7 +640,7 @@ class PRReview {
         }
       ];
 
-      if (entry.author == payload.user_id) {
+      if (entry.author == payload.user.id) {
         buttons.push({
           type: "button",
           text: {
@@ -505,305 +683,6 @@ class PRReview {
 
       blocks.push({ type: "divider" });
     });
-
-    // Returns result to user
-    app.client.views.open({
-      token: token,
-      trigger_id: payload.trigger_id,
-      view: {
-        type: "modal",
-        title: {
-          type: "plain_text",
-          text: `Open PR Review Requests`,
-          emoji: true
-        },
-        close: {
-          type: "plain_text",
-          text: "Close",
-          emoji: true
-        },
-        blocks: blocks
-      }
-    });
-  }
-
-  /*
-    Updates the request status according to the reaction received
-  */
-  static async computeReaction(event, client) {
-    const channel = event.item.channel;
-    if (event.reaction == REVIEWED || event.reaction == APPROVED) {
-      const dbEntry = await MongoDB.findPR(channel, event.item.ts);
-      if (dbEntry) {
-        client.chat.postMessage({
-          token: token,
-          channel: dbEntry.author,
-          text: `:${event.reaction}: Your PR was ${event.reaction} by <@${event.user}>`
-        });
-
-        switch (event.reaction) {
-          case REVIEWED:
-            await MongoDB.updateStatus(channel, dbEntry._id, REVIEWED);
-            break;
-          case APPROVED:
-            await MongoDB.updateStatus(channel, dbEntry._id, APPROVED);
-            break;
-        }
-      }
-    }
-  }
-
-  /*
-    Prepares object payload to send to update status when a button is pressed
-  */
-  static async takeAction(body, client) {
-    const event = {
-      reaction: body.actions[0].action_id.includes("review")
-        ? REVIEWED
-        : APPROVED,
-      item: {
-        ts: body.actions[0].value,
-        channel: body.channel.id
-      },
-      user: body.user.id
-    };
-    this.computeReaction(event, client);
-  }
-  
-  /*
-    Prepares object payload to send to update status when a button is pressed in modal
-  */
-  static async takeActionModal(body, client) {
-    const parsedValues = JSON.parse(body.actions[0].value)
-    body.channel = {
-      id: parsedValues.channel_id,
-      channel_name: parsedValues.channel_name
-    }
-    debugger
-    const event = {
-      reaction: body.actions[0].action_id.includes("review")
-        ? REVIEWED
-        : APPROVED,
-      item: {
-        ts: parsedValues.pr_post_id,
-        channel: body.channel.id
-      },
-      user: body.user.id
-    };
-    await this.computeReaction(event, client);
-    const viewBlocks = await createOpenReviewsViewBlock(body);
-    await client.views.update({
-      token: token,
-      view_id: body.view.id,
-      view: {
-        type: "modal",
-        title: {
-          type: "plain_text",
-          text: `Open PR Review Updated`,
-          emoji: true
-        },
-        close: {
-          type: "plain_text",
-          text: "Close",
-          emoji: true
-        },
-        blocks: viewBlocks
-      }
-    });
-  }
-
-  /*
-    Removes request from DB and sends message in thread to say the PR was merged
-  */
-  static async mergedPR(data, client) {
-    const isFromAppHome = data.container.type == "view";
-    const body = isFromAppHome ? AppHome.normalizeBody(data) : data;
-    const dbEntry = await MongoDB.findPR(
-      body.channel.id,
-      body.actions[0].value
-    );
-    if (!dbEntry) return; // If entry not found do nothing
-    if (dbEntry.author == body.user.id) {
-      await MongoDB.finalizePR(body.channel.id, body.actions[0].value);
-      client.chat.postMessage({
-        token: token,
-        text: ":checkered_flag: Pull Request merged. Thank you all!",
-        channel: body.channel.id,
-        thread_ts: body.actions[0].value
-      });
-      if (isFromAppHome) {
-        console.log("Should open the other view");
-        client.views.push({
-          token: token,
-          trigger_id: body.trigger_id,
-          view: {
-            type: "modal",
-            title: {
-              type: "plain_text",
-              text: "PR Review Complete",
-              emoji: true
-            },
-            close: {
-              type: "plain_text",
-              text: "Close",
-              emoji: true
-            },
-            blocks: [
-              {
-                type: "section",
-                text: {
-                  type: "mrkdwn",
-                  text:
-                    ":checkered_flag: Review request marked as *Merged* and removed from queue."
-                }
-              }
-            ]
-          }
-        });
-      }
-    } else {
-      client.chat.postEphemeral({
-        token: token,
-        text: ":octagonal_sign: Only the author can set as Merged",
-        channel: body.channel.id,
-        thread_ts: body.actions[0].value,
-        user: body.user.id
-      });
-    }
-  }
-}
-
-async function createOpenReviewsViewBlock(payload) {
-  debugger
-  const channel_id = payload.channel.id;
-  const channel_name = payload.channel.name;
-  const user_id = payload.user.id;
-  const data = await MongoDB.listChannelPRs(channel_id);
-  const blocks =
-    data.length > 0
-      ? []
-      : [
-          {
-            type: "section",
-            text: {
-              type: "plain_text",
-              text: `No open PR review requests for ${channel_name}`
-            }
-          },
-          {
-            type: "image",
-            image_url:
-              "https://media.giphy.com/media/26hkhPJ5hmdD87HYA/giphy.gif",
-            alt_text: "nothing"
-          }
-        ];
-
-  //Populates the block with entries from DB
-  data.forEach(entry => {
-    let emoji;
-    switch (entry.status) {
-      case "reviewed":
-        emoji = "reviewed";
-        break;
-      case "approved":
-        emoji = "approved";
-        break;
-      default:
-        emoji = "hourglass_flowing_sand";
-        break;
-    }
-
-    const createdAt = TimeFormatter.createdAt(entry.created_at, new Date());
-
-    blocks.push({
-      type: "section",
-      text: {
-        type: "mrkdwn",
-        text: `<@${entry.author}>'s ${entry.service} review request:`
-      }
-    });
-
-    blocks.push({
-      type: "section",
-      fields: [
-        {
-          type: "mrkdwn",
-          text: `*Status:*\n:${emoji}: \t${StringUtils.capitalizeFirstLetter(
-            entry.status
-          )}`
-        },
-        {
-          type: "mrkdwn",
-          text: `*Summary:*\n${entry.summary}`
-        },
-        {
-          type: "mrkdwn",
-          text: `*Created:*\n${createdAt}`
-        },
-        {
-          type: "mrkdwn",
-          text: `*Notes:*\n${entry.notes ? entry.notes : ""}`
-        }
-      ]
-    });
-
-    const buttons = [
-      {
-        type: "button",
-        text: {
-          type: "plain_text",
-          emoji: true,
-          text: ":eyes: Take a look "
-        },
-        value: "view",
-        url: `${entry.link}`,
-        action_id: "link-button-action"
-      }
-    ];
-
-    if (entry.author == payload.user_id) {
-      buttons.push({
-        type: "button",
-        text: {
-          type: "plain_text",
-          emoji: true,
-          text: ":white_check_mark: Merged"
-        },
-        style: "primary",
-        value: entry.pr_post_id,
-        action_id: "merged-button-action"
-      });
-    } else {
-      buttons.push({
-        type: "button",
-        text: {
-          type: "plain_text",
-          emoji: true,
-          text: ":approved: Approve"
-        },
-        style: "primary",
-        value: entry.pr_post_id,
-        action_id: "approve-pr-action"
-      });
-      buttons.push({
-        type: "button",
-        text: {
-          type: "plain_text",
-          emoji: true,
-          text: ":reviewed: Review"
-        },
-        style: "danger",
-        value: entry.pr_post_id,
-        action_id: "review-pr-action"
-      });
-    }
-    blocks.push({
-      type: "actions",
-      elements: buttons
-    });
-
-    blocks.push({ type: "divider" });
-  });
   return blocks;
 }
 
